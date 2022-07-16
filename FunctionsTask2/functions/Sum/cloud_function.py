8f1af4fd920131037d4e8764724a206cb3aa98bf
sajadkh <sajadkhosravi1996@gmail.com>
2022-07-16 15:41:35 -0700

    Fix sum function task2

--- FunctionsTask2/functions/Sum/cloud_function.py
+++ FunctionsTask2/functions/Sum/cloud_function.py
@@ -16,15 +16,16 @@ def cloud_function(json_input):
     for i in range(len(count_arr)):
         new_sum_value = sum_value + count_arr[i]
         if new_sum_value > threshold and sum_value == threshold:
+            j = i
             break
         elif new_sum_value > threshold:
             must_be_partial_modified_str = batch_arr[i]
             must_be_partial_modified_remained_pattern = threshold - sum_value
-            i = i + 1
+            j = i + 1
             break
         sum_value = new_sum_value
     total_count = sum(count_arr)
-    must_be_modified_batch_arr = batch_arr[i:]
+    must_be_modified_batch_arr = batch_arr[j:]
     un_modified_batch_arr = batch_arr[0:i]
     # return the result
     res = {
